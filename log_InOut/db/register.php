c236725f61c7261d42f4068a4ce50ccf9af85f39
Tomáš Frenzel <D20331@oaopava.cz>
2024-01-24 10:42:21 +0100

    siashaiugs

--- log_InOut/db/register.php
+++ log_InOut/db/register.php
@@ -39,38 +39,4 @@ if (isset($_POST["submit"])) {
         echo 'Některé povinné pole nebylo vyplněno.';
     }
 }
-
-
-
-
-
-/*if (isset($_POST["submit"])){
-  echo 'data odeslány';
-    
-    $email = $_POST["email"];
-    $password = $_POST["password"];
-    $password2 = $_POST["password2"];
-
-    if ($password == $password2){
-        echo 'bum';
-        $query = "INSERT INTO users(email,password) 
-        VALUES('$email','$password')";
-
-        $result = mysqli_query($connection, $query);
-
-        if(!$result){
-            die ("dotaz do databze selhal".mysqli_error());
-        }
-    }else{
-        echo 'zadali jste spatné heslo';
-    }
-    
-    
-
-    Undefined array key "password2" in C:\xampp\htdocs\PVA\phpTesty\log_InOut\db\register.php on line 8
-
-
-}
-*/
-
 ?>
