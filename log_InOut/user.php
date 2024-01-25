8171e9e5173cbd2e90c18e526993b06798cdfaee
Tomáš Frenzel <D20331@oaopava.cz>
2024-01-25 15:30:10 +0100

    logout

--- log_InOut/user.php
+++ log_InOut/user.php
@@ -11,13 +11,24 @@ require_once "template/templateHeader.php";
 *
 */ 
 
+
+if (isset($_POST["submit"])){
+
+    session_destroy();
+    header ('location: register.php');
+    }
+    
+
 echo 'Email uživatele '. $_SESSION["email"];
 
 
 
-echo"
+echo"xxx
 <body>
     <H2>jste přihlášen pane $username</H2>
+    <form action='user.php' method='post'>
+    <input type='submit' name'logout'value='ODHLÁSIT SE'>
+</form>
 </body>
 </html>
 ";
